5e4063edd335f1e7208592fb20aff9102f8e64b6
Travis Flatt <tflatt74@gmail.com>
2014-06-26 11:13:07 -0500

    post editing

--- collections/posts.js
+++ collections/posts.js
@@ -1,8 +1,35 @@
 Posts = new Meteor.Collection('posts');
 
-Posts.allow({
-	insert: function(userId, doc) {
-		// only allow posting if logged in
-		return !! userId;
+Meteor.methods({
+	post: function(postAttributes) {
+		var user = Meteor.user(),
+			postWithSameLink = Posts.findOne({url: postAttributes.url});
+
+		// ensure user logged in
+		if(!user)
+			throw new Meteor.Error(401, "You need to login to post new stories");
+
+		// ensure post has a title
+		if(!postAttributes.title)
+			throw new Meteor.Error(422, 'Please fill in a headline');
+
+		// check that there are no previous posts with same link
+
+		if(postAttributes.url && postWithSameLink) {
+			throw new Meteor.Error(302,
+				'This link has already been posted',
+				postWithSameLink._id);
+		}
+
+		// pick out whitelisted keys
+		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
+			userId: user._id,
+			author: user.username,
+			submitted: new Date().getTime()
+		});
+
+		var postId = Posts.insert(post);
+
+		return postId;
 	}
 });
