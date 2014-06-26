2aa10a0ee486e876da3c76216f115b0e5646f09b
Travis Flatt <tflatt74@gmail.com>
2014-06-26 16:55:15 -0500

    reordering animation

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,5 +1,9 @@
-// Template.postsList.helpers({
-// 	posts: function() {
-// 		return Posts.find({}, {sort: {submitted: -1}});
-// 	}
-// });
+Template.postsList.helpers({
+	postsWithRank: function() {
+		this.posts.rewind();
+		return this.posts.map(function(post, index, cursor) {
+			post._rank = index;
+			return post;
+		});
+	}
+});
