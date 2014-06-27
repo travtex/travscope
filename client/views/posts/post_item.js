dedecf008d87e3361f550d114c25878a7b1fd72b
Travis Flatt <tflatt74@gmail.com>
2014-06-27 09:21:40 -0500

    bug fixes

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -24,10 +24,12 @@ Template.postItem.helpers({
 		var newPosition = post._rank * POST_HEIGHT;
 		var attributes = {};
 
-		if(! _.isUndefined(post.position)) {
-			var offset = post.position - newPosition;
-			attributes.style = "top: " + offset + "px";
-			if(offset === 0)
+		if(_.isUndefined(post.position)) {
+			attributes.class = 'post invisible';
+		} else {
+			var delta = post.position - newPosition;
+			attributes.style = "top: " + delta + "px";
+			if(delta === 0)
 				attributes.class = "post animate"
 		}
 
